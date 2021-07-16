0e9a6de14f7b4789c58efe054b85fe944605de2a
Kim - leo <ochell2@naver.com>
2021-07-16 12:17:13 +0900

    Initial Commit

---
+++ 210716Memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  210716Memo
+//
+//  Created by 김승현 on 2021/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
